67a4054cf9d2fb78a5dc98c11d08e093ae3c8723
Yashwanth <yashwanth051202@gmail.com>
2023-10-03 14:47:04 +0530

    the version with internet connectivity status display

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -6,6 +6,10 @@ import '../models/blog.dart';
 import 'package:http/http.dart' as http;
 
 import '../widgets/blogWidgetInHomeScreen.dart';
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:get/get.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -19,7 +23,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   void initState() {
     super.initState();
-    fetchPosts();
+    // fetchPosts();
   }
 
   Future<void> fetchPosts() async {
@@ -74,17 +78,28 @@ class _HomeScreenState extends State<HomeScreen> {
                   Stack(
                     children: [
                       Container(
-                        padding: EdgeInsets.only(top: 60, left: 25),
                         height: 140,
-                        width: wi,
-                        // color: Color.fromRGBO(18, 18, 18, 1),
                         color: Color.fromRGBO(40, 40, 40, 1),
-                        child: Text(
-                          'Blogs',
-                          style: TextStyle(
-                              fontSize: 30,
-                              fontWeight: FontWeight.w700,
-                              color: Colors.white),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Container(
+                              padding: EdgeInsets.only(top: 60, left: 25),
+                              height: 140,
+                              // width: wi,
+                              // color: Color.fromRGBO(18, 18, 18, 1),
+                              color: Color.fromRGBO(40, 40, 40, 1),
+                              child: Text(
+                                'Blogs',
+                                style: TextStyle(
+                                    fontSize: 30,
+                                    fontWeight: FontWeight.w700,
+                                    color: Colors.white),
+                              ),
+                            ),
+                            ConnectivityStatusWidget(),
+                          ],
                         ),
                       ),
                       Positioned(
@@ -120,21 +135,24 @@ class _HomeScreenState extends State<HomeScreen> {
                           ),
                         )
                       : Container(
-                          // color: Color.fromRGBO(40, 40, 40, 1),
-                          color: Color.fromRGBO(18, 18, 18, 1),
-                          height: posts.length * 260,
-                          child: ListView.builder(
-                            physics: NeverScrollableScrollPhysics(),
-                            itemCount: posts.length,
-                            itemBuilder: (context, index) {
-                              // return ListTile(
-                              //   title: Text(posts[index].id),
-                              //   subtitle: Text(posts[index].imageUrl),
-                              // );
-                              return CardWidget(posts[index], posts[index].id);
-                            },
-                          ),
+                          child: Text('helllo there where '),
                         ),
+                  // Container(
+                  //     // color: Color.fromRGBO(40, 40, 40, 1),
+                  //     color: Color.fromRGBO(18, 18, 18, 1),
+                  //     height: posts.length * 260,
+                  //     child: ListView.builder(
+                  //       physics: NeverScrollableScrollPhysics(),
+                  //       itemCount: posts.length,
+                  //       itemBuilder: (context, index) {
+                  //         // return ListTile(
+                  //         //   title: Text(posts[index].id),
+                  //         //   subtitle: Text(posts[index].imageUrl),
+                  //         // );
+                  //         return CardWidget(posts[index], posts[index].id);
+                  //       },
+                  //     ),
+                  //   ),
                 ],
               ),
             ),
@@ -142,5 +160,73 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
       ),
     );
+  }
+}
+
+class ConnectivityController extends GetxController {
+  Rx<ConnectivityResult> connectivityResult =
+      Rx<ConnectivityResult>(ConnectivityResult.none);
+
+  void checkConnectivity() async {
+    final result = await Connectivity().checkConnectivity();
+    connectivityResult.value = result;
+  }
+
+  @override
+  void onInit() {
+    super.onInit();
+    // Initialize the connectivity check and set it in the state
+    checkConnectivity();
+
+    // Periodically check connectivity (e.g., every 5 seconds)
+    Timer.periodic(Duration(seconds: 1), (_) {
+      checkConnectivity();
+    });
+  }
+}
+
+class ConnectivityStatusWidget extends StatefulWidget {
+  @override
+  State<ConnectivityStatusWidget> createState() =>
+      _ConnectivityStatusWidgetState();
+}
+
+class _ConnectivityStatusWidgetState extends State<ConnectivityStatusWidget> {
+  final ConnectivityController controller = Get.put(ConnectivityController());
+
+  @override
+  Widget build(BuildContext context) {
+    return Obx(() {
+      final connectivityResult = controller.connectivityResult.value;
+      Icon icon;
+      Color iconColor;
+
+      // Check the connectivity status and set the icon accordingly
+      switch (connectivityResult) {
+        case ConnectivityResult.none:
+          icon = Icon(Icons.signal_wifi_off);
+          iconColor = Colors.red;
+          break;
+        case ConnectivityResult.wifi:
+        case ConnectivityResult.mobile:
+          icon = Icon(Icons.signal_wifi_4_bar);
+          iconColor = Colors.green;
+          break;
+        default:
+          icon = Icon(Icons.warning);
+          iconColor = Colors.yellow;
+          break;
+      }
+
+      return Container(
+        color: Color.fromRGBO(40, 40, 40, 1),
+        margin: EdgeInsets.only(right: 20, top: 15),
+        child: Icon(
+          icon.icon,
+          color: iconColor,
+          size: 30,
+        ),
+      );
+    });
   }
 }
